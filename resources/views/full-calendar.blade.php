4448dec8b77a2035b1c17d24fdbf974849822767
Panos Saridis <saridispanos@gmail.com>
2021-09-28 03:42:55 +0300

    imerologio

--- resources/views/full-calendar.blade.php
+++ resources/views/full-calendar.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 
     <div class="container mt-5" style="max-width: 700px">
-        <h2 class="h2 text-center mb-5 border-bottom pb-3">Test </h2>
+        <h2 class="h2 text-center mb-5 border-bottom pb-3">Ημερολόγιο</h2>
         <div id='full_calendar_events'></div>
     </div>
 @endsection
