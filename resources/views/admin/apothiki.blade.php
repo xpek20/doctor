f9b45be01a778f017e11cf6157468513a9d1a6d2
Panos Saridis <saridispanos@gmail.com>
2021-11-01 16:58:55 +0200

    1.11

--- resources/views/admin/apothiki.blade.php
+++ resources/views/admin/apothiki.blade.php
@@ -3,107 +3,149 @@
 @section('content')
 
 <div class="container-fluid">
-	<div class="row">
-	
-    <div class="col-md-4">
-			<h2>
-				Φάρμακα
-			</h2>
-			<p>
-				<a class="btn  btn-success" href='/admin/medicine/create'>Προσθήκη »</a>
-			</p> 
-		</div>
-    
-		<div class="col-md-4">
-			<h2>
-				Φαγητό
-			</h2>
-			<p>
-				<a class="btn  btn-success" href='/admin/food/create'>Προσθήκη »</a>
-			</p> 
-		</div>
-		
-		<div class="col-md-4">
-			<h2>
-				Αναλώσιμα
-			</h2>
-			
-			<p>
-				<a class="btn  btn-success" href='/admin/consumable/create'>Προσθήκη »</a>
-			</p>
-		</div>
-	</div>
-
-  <div class="row">
-	
-    <div class="col-md-4">
-			<h2>
-				Οροί
-			</h2>
-			<p>
-				<a class="btn  btn-success" href='/admin/oros/create'>Προβολή »</a>
-			</p> 
-		</div>
-    
-		<div class="col-md-4">
-			<h2>
-				Εξετάσεις αίματος
-			</h2>
-			<p>
-				<a class="btn  btn-success" href='/admin/eksetasi-aimatos/create'>Προσθήκη »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Χαρτικά
-			</h2>
-			<p>
-				<a class="btn  btn-success" href='/admin/xartika/create'>Προσθήκη »</a>
-			</p>
-		</div>
-	</div>
-
     <div class="row">
-	
+
         <div class="col-md-4">
-                <h2>
-                    Γάλατα
-                </h2>
-                <p>
-                    <a class="btn  btn-success" href='/admin/galata/create'>Προβολή »</a>
-                </p> 
-            </div>
-        
-            <div class="col-md-4">
-                <h2>
-                    Pampers
-                </h2>
-                <p>
-                    <a class="btn  btn-success" href='/admin/pampers/create'>Προσθήκη »</a>
-                </p>
-            </div>
-            <div class="col-md-4">
-                <h2>
-                    Medelas
-                </h2>
-                <p>
-                    <a class="btn  btn-success" href='/admin/medelas/create'>Προσθήκη »</a>
-                </p>
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Φάρμακα
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/medicine/create'>Προσθήκη »</a>
+                    </p>
+                </div>
             </div>
         </div>
 
-        <div class="row">
-	
-            <div class="col-md-6">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Φαγητό
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/food/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Αναλώσιμα
+                    </h2>
+
+                    <p>
+                        <a class="btn  btn-success" href='/admin/consumable/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Οροί
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/oros/create'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Εξετάσεις αίματος
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/eksetasi-aimatos/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Χαρτικά
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/xartika/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Γάλατα
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/galata/create'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Pampers
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/pampers/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Medelas
+                    </h2>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/medelas/create'>Προσθήκη »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
                     <h2>
                         Βελόνες - Σύριγγες
                     </h2>
                     <p>
                         <a class="btn  btn-success" href='/admin/velones-sirigges/create'>Προβολή »</a>
-                    </p> 
+                    </p>
                 </div>
-            
-                <div class="col-md-6">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
                     <h2>
                         Ράμματα
                     </h2>
@@ -112,6 +154,8 @@
                     </p>
                 </div>
             </div>
+        </div>
+    </div>
 
 </div>
 
--- resources/views/admin/esoda-eksoda.blade.php
+++ resources/views/admin/esoda-eksoda.blade.php
@@ -2,101 +2,105 @@
 
 @hasanyrole('Admin|AdminB|Γραμματεία')
 @section('content')
-<style> 
-.card {
-  background: #fff;
-  border-radius: 2px;
-  display: inline-block;
-  height: auto;
-  margin: 1rem;
-  position: relative;
-  
-}
-.card-3 {
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-}
 
 
-</style>
+<div class="container-fluid vertical-center">
+    <div class="row">
 
-<div class="container-fluid vertical-center" >
-	<div class="row">
-	
-    <div class="col-md-4 card card-3">
-			<h2 style="margin-top:10px">
-				Έσοδα
-			</h2>
-			<p>
-				Προσθήκη εσόδου
-			</p>
-			<p>
-				<a class="btn  btn-success" href='/admin/income/create'>Προσθήκη »</a>
-				<a style="margin-left:10px" class="btn  btn-info" href='/admin/income'>Προβολή »</a>
-			</p> 
-		</div>
-    
-		<div class="col-md-4 card card-3">
-			<h2 style="margin-top:10px">
-				Κατηγορία Έσοδων
-			</h2>
-			<p>
-				Προσθήκη κατηγορίας Έσοδων
-			</p>
-			<p>
-				<a class="btn  btn-success" href='/admin/income-category/create'>Προσθήκη »</a>
-				<a style="margin-left:10px" class="btn  btn-info" href='/admin/income-category'>Προβολή »</a>
-			</p>
-		</div>
-	</div>
-  <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 style="margin-top:10px">
+                        Έσοδα
+                    </h2>
+                    <p>
+                        Προσθήκη εσόδου
+                    </p>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/income/create'>Προσθήκη »</a>
+                        <a style="margin-left:10px" class="btn  btn-info" href='/admin/income'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
 
-	<div class="col-md-4 card card-3">
-		<h2 style="margin-top:10px">
-			Έξοδα
-		</h2>
-		<p>
-			Προσθήκη Εξόδων 
-		</p>
-		<p>
-			<a class="btn  btn-success" href='/admin/expense/create'>Προσθήκη »</a>
-			<a style="margin-left:10px" class="btn  btn-info" href='/admin/expense'>Προβολή »</a>
-		</p>
-	</div>
-	
-	<div class="col-md-4 card card-3">
-		<h2 style="margin-top:10px">
-			Κατηγορίες Εξόδων
-		</h2>
-		<p>
-			Προσθήκη κατηγορίας Εξόδων 
-		</p>
-		<p>
-			<a class="btn  btn-success" href='/admin/expense-category/create'>Προσθήκη »</a>
-			<a style="margin-left:10px" class="btn  btn-info" href='/admin/expense-category'>Προβολή »</a>
-		</p>
-	</div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 style="margin-top:10px">
+                        Κατηγορία Έσοδων
+                    </h2>
+                    <p>
+                        Προσθήκη κατηγορίας Έσοδων
+                    </p>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/income-category/create'>Προσθήκη »</a>
+                        <a style="margin-left:10px" class="btn  btn-info" href='/admin/income-category'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
 
-    
-    
-		
-	</div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 style="margin-top:10px">
+                        Έξοδα
+                    </h2>
+                    <p>
+                        Προσθήκη Εξόδων
+                    </p>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/expense/create'>Προσθήκη »</a>
+                        <a style="margin-left:10px" class="btn  btn-info" href='/admin/expense'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
 
-	<div class="row">
-		<div class="col-md-4 card card-3">
-			<h2 style="margin-top:10px">
-				Σύνοψη Εσόδων - Εξόδων
-			</h2>
-			<p>
-				
-			</p>
-			<p>
-				<a class="btn  btn-info" href='/admin/expense-reports'>Προβολή »</a>
-				
-			</p>
-		</div>
-	</div>
-	
-</div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 style="margin-top:10px">
+                        Κατηγορίες Εξόδων
+                    </h2>
+                    <p>
+                        Προσθήκη κατηγορίας Εξόδων
+                    </p>
+                    <p>
+                        <a class="btn  btn-success" href='/admin/expense-category/create'>Προσθήκη »</a>
+                        <a style="margin-left:10px" class="btn  btn-info" href='/admin/expense-category'>Προβολή »</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h2 style="margin-top:10px">
+                        Σύνοψη Εσόδων - Εξόδων
+                    </h2>
+                    <p>
+
+                    </p>
+                    <p>
+                        <a class="btn  btn-info" href='/admin/expense-reports'>Προβολή »</a>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 @endsection
 
