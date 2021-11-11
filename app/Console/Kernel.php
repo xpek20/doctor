60346720bd42eadff297de72478bbe2bfb60bc84
Panos Saridis <saridispanos@gmail.com>
2021-11-11 15:40:55 +0200

    backup_4

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:run --only-db')->daily()->at('01:30');
+        $schedule->command('backup:run --only-db')->hourly();
     }
 
     /**
